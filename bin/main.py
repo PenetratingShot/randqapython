dff13791215aad45ab1675ecbbc82ef26e16cd47
PenetratingShot <shadowtemplates@gmail.com>
2017-08-18 12:16:41 -0700

    options

--- bin/main.py
+++ bin/main.py
@@ -1,6 +1,8 @@
 import random
 
 # input options 1 for now
-def input() {
-  print("All options are CASE SENSITIVE. Please be careful when typing them in.
-}
+def input():
+  print("All options are CASE SENSITIVE! Besure to type your options in carefully.")
+
+  def options():
+      availible_options = [" What do you do?, What are you good for?, Can you do math?, Are you an idiot?"]
