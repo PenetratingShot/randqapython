9c18fcdcbd8b072462367816549e37b2c0bda26e
Shreyas Lad <shadowtemplates@gmail.com>
2017-08-21 12:15:15 -0700

    Update main.py

--- bin/main.py
+++ bin/main.py
@@ -1,8 +1,17 @@
-import random
+err = ("An error occured... Now stop talking to a computer and get a life")
 
 # input options 1 for now
-def input():
-  print("All options are CASE SENSITIVE! Besure to type your options in carefully.")
-
-  def options():
-      availible_options = [" What do you do?, What are you good for?, Can you do math?, Are you an idiot?"]
+def usr_input():
+  print("This is called an intro \n")
+  print("All options are CASE SENSITIVE! Besure to type your options in carefully. \n")
+  availible_options = print(" What do you do?", "What are you good for?", "Can you do math?", "Are you an idiot?")
+  posted_available_options = "What do you do?, What are you good for?"
+  print(posted_available_options)
+  selected = input("Type in 1 answer...")
+  
+  if selected == "What do you do?":
+    print("\n I just take up memory. Plus, if you're talking to me, you have no life.")
+  else:
+    print(err)
+    
+usr_input()
